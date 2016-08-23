0c351c09ec21cec2b522d5f17567f3dc30d8d196
steve280782 <mail@s-winwood.com>
2016-08-23 21:53:27 +0100

    Added noughts_and_crosses.py

---
+++ noughts_and_crosses.py
@@ -0,0 +1,33 @@
+# Noughts and crosses game using dictionaries
+
+# Create a blank board
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', \
+           'low-M': ' ', 'low-R': ' '}
+
+completeBoard = {'top-L': 'top-L', 'top-M': 'top-M', 'top-R': 'top-R', \
+                 'mid-L': 'mid-L', 'mid-M': 'mid-M', 'mid-R': 'mid-R', \
+                 'low-L': 'low-L', 'low-M': 'low-M', 'low-R': 'low-R'}
+
+# Function to print board to screen
+def printboard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+printboard(completeBoard)
+print('\r')
+# X goes first then alternates with O
+turn = 'X'
+# There are 9 goes
+for i in range(9):
+    printboard(theBoard)
+    print('Turn for ' + turn + '. Move on which space?')
+    move = input()
+    theBoard[move] = turn
+    if turn == 'X':
+        turn = 'O'
+    else:
+        turn = 'X'
+printboard(theBoard)
