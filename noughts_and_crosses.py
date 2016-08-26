c241e02f0090200b63eb6f02639f716f5a0e9459
steve280782 <mail@s-winwood.com>
2016-08-26 22:42:08 +0100

    Added validaton of user input Moved quit option to new validation function

--- noughts_and_crosses.py
+++ noughts_and_crosses.py
@@ -4,9 +4,13 @@ import sys
 theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', \
            'low-M': ' ', 'low-R': ' '}
 
+# Create finished board for demo
 completeBoard = {'top-L': 'top-L', 'top-M': 'top-M', 'top-R': 'top-R',
                  'mid-L': 'mid-L', 'mid-M': 'mid-M', 'mid-R': 'mid-R',
                  'low-L': 'low-L', 'low-M': 'low-M', 'low-R': 'low-R'}
+
+# Tuple used for validation of user inputs
+validation = ('q', 'top-L', 'top-M', 'top-R', 'mid-L', 'mid-M', 'mid-R', 'low-L', 'low-M', 'low-R')
 
 # Function to print board to screen
 def printboard(board):
@@ -16,25 +20,34 @@ def printboard(board):
     print('-+-+-')
     print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
-# Print a sample board with references
-printboard(completeBoard)
-print('\r')
+# Function to valide user inputs
+def validate(userinput):
+    while userinput not in validation:
+        print('invalid entry, try again')
+        userinput=input()
+    if userinput == 'q':
+        sys.exit()
+    return(userinput)
 
-# X goes first then alternates with O
+# Game starts, X goes first then alternates with O
 turn = 'X'
 # There are 9 goes
 for i in range(9):
-    printboard(theBoard)
+    printboard(completeBoard)
+    print('\r')
     print('Turn for ' + turn + '. Move on which space?\n(press q to quit)')
     move = input()
-    if move == 'q':
-        sys.exit()
+    # Call validation function and pass the user input via the move variable
+    checkedmove = validate(move)
+    # Validated user input is returned
+    theBoard[checkedmove] = turn
+    printboard(theBoard)
+    print('\r')
+    # Alternate to next turn
+    if turn == 'X':
+        turn = 'O'
     else:
-        theBoard[move] = turn
-        if turn == 'X':
-            turn = 'O'
-        else:
-            turn = 'X'
-# printboard(theBoard)
+        turn = 'X'
+# At the end exit
 else:
     sys.exit()
