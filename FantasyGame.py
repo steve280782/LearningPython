4e205338ea1715de4c27e9df29c219399a999797
steve280782 <mail@s-winwood.com>
2016-08-28 22:35:18 +0100

    in progress updates to create addToInventory function

--- FantasyGame.py
+++ FantasyGame.py
@@ -5,7 +5,18 @@ def displayInventory(stuff):
         print (v, k)
     print ('Total number of items:', sum(stuff.values()))
 
+def addToInventory(stuff, addedItems):
+    for k in addedItems:
+        if k in stuff.keys():
+            #stuff[k] = stuff.values() + 1
+            print(k)
+
 # Inventory dictionary
 inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+
 # Call function
-displayInventory(inventory)
+#displayInventory(inventory)
+addToInventory(inventory, dragonLoot)
